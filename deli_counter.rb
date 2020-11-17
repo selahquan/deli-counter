9fe0cefe2375784fc5042a7d841a576123d87250
Josh <josh.handon@gmail.com>
2020-11-16 20:56:51 -0500

    testing a saved between machines

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,22 @@
 # Write your code here.
+katz_deli = []
+
+def line(array)
+    if array == []
+        puts "The line is currently empty."
+    else
+        #create new array & shovel "index + 1" & name, then add new array to end of the puts string
+        array.each do |index, name|
+        puts "The line is currently: #{new_array}"
+    end
+end
+
+def take_a_number(array, name)
+    array << name
+    puts "#{name} #{array.index}"
+end
+
+def now_serving
+
+end
+end
