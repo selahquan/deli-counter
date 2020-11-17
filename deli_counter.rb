de8acacab9986141052065f6b62512d6edcbcc31
selahquan <josh.handon@gmail.com>
2020-11-17 14:14:37 -0500

    commit tree df4fd8c981eb17852bed27b40444c00362e1978b

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,33 +2,29 @@
 katz_deli = []
 
 def line(array)
-    if array == []
+    phrase = "The line is currently:"
+    if array.count == 0
         puts "The line is currently empty."
-    elsif array.length > 0
+    else
         #create new array & shovel "index + 1" & name, then add new array to end of the puts string
-        new_array = []
-        array.each_with_index(0) do |index, name|
-        puts "The line is currently: #{new_array.each_with_index}"
+        array.each_with_index do |name, index|
+        phrase += " #{index +1}. #{name}"
         end
+        puts phrase
     end
-    return array.each_with_index
 end
 
 def take_a_number(array, new_name)
     array << new_name
-    if array == []
-        puts "Welcome, #{new_name}. You are number 1 in line."
-    elsif array.size > 1
-        puts "Welcome, #{new_name}. You are number #{array.index new_name + 1} in line."
-    end
+    puts "Welcome, #{new_name}. You are number #{array.length} in line."
 end
 
 def now_serving(array)
     if array == []
         puts "There is nobody waiting to be served!"
-    elsif array.size > 0
-        serving = array[0]
-        array.shift(0)
+    else array.size > 0
+        puts "Currently serving #{array[0]}."
+    end
+    array.shift
         
     end
-end
